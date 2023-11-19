2f82c216a06bccb28dd34c380e885912267fe7f5
sai charan reddy vangala <vangala.38@wright.edu>
2023-11-18 21:15:08 -0500

    Backend-routes

--- Backend/api-endpoint/models/login.py
+++ Backend/api-endpoint/models/login.py
@@ -1,23 +1,27 @@
-from boto3 import resource
+# importing nessorcy packages and config file that contains keys for connecting to aws 
+from boto3 import resource 
 import config
 
-
+# creating instances of dynamo db
 resource = resource(
     'dynamodb',
     aws_access_key_id     = config.AWS_ACCESS_KEY_ID,
     aws_secret_access_key = config.AWS_SECRET_ACCESS_KEY,
     region_name           = config.REGION_NAME
-)
+) 
+# referencing to Logininfo table in dynamo db
 login_info = resource.Table('LoginInfo')
 
+# retriving password of user stored in db
 def user_authentication(email):
     response = login_info.get_item(
         Key = {
             'email':email
         },
         AttributesToGet = [
-            'password' # valid types dont throw error, 
-        ]                      # Other types should be converted to python type before sending as json response
+            'password' 
+        ]                      
     )
     print(response)
+    # returing responce
     return response
